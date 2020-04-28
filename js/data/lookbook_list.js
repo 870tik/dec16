1bcd23b6038a2c5a194335c1d95c94c1ceb25482
Shota Nemoto <>
2020-04-28 23:01:16 +0900

    Replace Images

--- js/data/lookbook_list.js
+++ js/data/lookbook_list.js
@@ -4,18 +4,6 @@ var lookbook = [
         "thumnail": "../data/lookbook/jessica/lookbook1.jpg",
         "name": "Jessica",
         "date": "2020/04",
-        "photonum":17
-    },{
-        "id" : "jessica",
-        "thumnail": "../data/lookbook/jessica/lookbook2.jpg",
-        "name": "Jessica",
-        "date": "2020/04",
-        "photonum":7
-    },{
-        "id" : "jessica",
-        "thumnail": "../data/lookbook/jessica/lookbook8.jpg",
-        "name": "Jessica",
-        "date": "2020/04",
         "photonum":3
     }
 ];
